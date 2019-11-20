acda2ea09a7602d9ccf8233f41c1cf151af44215
Maciej Kozik <m.kozik@iambot.ai>
2019-11-20 15:25:00 +0100

    create diets query

--- frontend/src/views/DashboardView.tsx
+++ frontend/src/views/DashboardView.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { MainNavbar } from 'src/components/Navbar'
 import HomePage from '../components/HomePage/HomePage'
+import { Route } from 'react-router-dom'
+import { DietsView } from './DietsView'
 
 const DashboardView = () => (
   <React.Fragment>
     <MainNavbar />
-    <HomePage />
+    <Route path="/" exact={true}>
+      <HomePage />
+    </Route>
+    <Route path="/diets">
+      <DietsView />
+    </Route>
   </React.Fragment>
 )
 
---
+++ frontend/src/views/DietsView.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { MainNavbar } from 'src/components/Navbar'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const DietsView = () => (
+  <Container>
+    <Row>
+      <Col></Col>
+    </Row>
+  </Container>
+)
+
+export { DietsView }
