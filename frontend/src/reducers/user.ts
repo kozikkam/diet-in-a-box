254a45d65e701e8ef36294355401132722e70f89
Kamil <ase495@gmail.com>
2019-11-24 19:03:29 +0100

    CR

--- frontend/src/reducers/user.ts
+++ frontend/src/reducers/user.ts
@@ -3,13 +3,12 @@ import * as actions from 'src/actions'
 
 export const userReducer = (
   state = {
-    loggedInUser: localStorage.getItem('user') as Nullable<User>,
+    loggedInUser: null as Nullable<User>,
   },
   action: actions.ActionType
 ) => {
   switch (action.type) {
     case actions.SET_LOGGED_IN_USER: {
-      localStorage.setItem('user', JSON.stringify(action.payload))
       return {
         ...state,
         loggedInUser: action.payload,
